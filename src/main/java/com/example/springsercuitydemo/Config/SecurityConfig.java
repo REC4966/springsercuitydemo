5aefd656a9560d01a5d26d44383358ae14dd33a0
xxx <1925148106@qq.com>
2023-05-27 22:44:38 +0800

    简短修改

--- src/main/java/com/example/springsercuitydemo/Config/SecurityConfig.java
+++ src/main/java/com/example/springsercuitydemo/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +17,8 @@ import com.example.springsercuitydemo.Exceptionhandler.AccessDeniedHandlerImpl;
 import com.example.springsercuitydemo.Exceptionhandler.AuthenticationEntryPointImpl;
 
 @Configuration
-// 开启权限注解 EnableGlobalMethodSecurity(prePostEnabled = true)
+// 开启权限注解 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
